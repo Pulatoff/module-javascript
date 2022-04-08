2a357373552c6592572e772f07ca7c631649072c
Niyozbk <niyozbekpulatov@gmail.com>
2022-04-08 17:23:37 +0500

    project started

---
+++ login.js
@@ -0,0 +1,14 @@
+// Named Export
+
+// let a = 2;
+// let t = 10;
+
+// export { a as b, t };
+
+// Default export
+
+// export default function (login, parol) {
+//   if (parol === 1111 && login === 'niyoz') {
+//     console.log('welcome to home');
+//   }
+// }
---
+++ script.js
@@ -0,0 +1,30 @@
+// import * as hammasi from './login.js';
+// console.log(hammasi);
+
+// Deafault import
+// import login from './login.js';
+// login('niyoz', 1111);
+
+// console.log('nma gap uzi');
+
+// let data = await fetch('https://restcountries.com/v2/name/uzbekistan');
+
+// console.log(data);
+
+// console.log('nioyzbek');
+
+function kerak() {
+  let cart = [];
+  let maxsulotlarSoni = 100;
+  let ishchikarSoni = 10;
+  let hamaPul = 1_000_000;
+  function sotibOl(nomi, soni) {
+    cart.push({ nomi, soni });
+    console.log(`Siz ${nomi}ni ${soni} ta oldingiz`);
+  }
+  return { sotibOl, ishchikarSoni, hamaPul };
+}
+
+let sot = kerak();
+console.log(sot);
+sot.sotibOl('olma', 100);
